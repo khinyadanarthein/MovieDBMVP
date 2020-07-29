afc1fe13cc65225f3318f9dbb6758818ded6b9b5
khinyadanarthein <khinyadanarthein6107@gmail.com>
2020-07-29 13:52:48 +0630

    Initial Commit

---
+++ MovieDBMVP/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieDBMVP
+//
+//  Created by Khin Yadanar Thein on 29/07/2020.
+//  Copyright © 2020 Khin Yadanar Thein. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MovieDBMVPTests/MovieDBMVPTests.swift
@@ -0,0 +1,34 @@
+//
+//  MovieDBMVPTests.swift
+//  MovieDBMVPTests
+//
+//  Created by Khin Yadanar Thein on 29/07/2020.
+//  Copyright © 2020 Khin Yadanar Thein. All rights reserved.
+//
+
+import XCTest
+@testable import MovieDBMVP
+
+class MovieDBMVPTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
